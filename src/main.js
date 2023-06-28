208883ba938faff0a9878d4fb8c38e99d91dac57
sleepsheep <sleepsheep@163.com>
2023-06-28 08:17:38 +0800

    login-static

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//引入elementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//图标 在assets文件夹中
+import '@/assets/icon/iconfont.css'
+import '@/assets/base.scss'
+//引入cookies
+import  VueCookies  from 'vue-cookies'
+
 const app = createApp(App)
 
 app.use(router)
+//别忘记element引入后需要use
+app.use(ElementPlus)
 
 app.mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login-body">
+    <h1>你好世界</h1>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue"
+</script>
+
+<style lang="scss" scoped>
+</style>
