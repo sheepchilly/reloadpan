b40a5b5f26d5d4099c510a06da8e0d5a57aaacbf
sleepsheep <sleepsheep@163.com>
2023-06-29 08:21:48 +0800

    add - LoginStatic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-body">
-    <h1>你好世界</h1>
+    <div class="bg"></div>
   </div>
 </template>
 
@@ -9,4 +9,24 @@ import {ref} from "vue"
 </script>
 
 <style lang="scss" scoped>
+.login-body{
+  width: 100%;
+  height: calc(100vh);
+  background-size: cover;
+  background: url("../assets/img/BackGroundLogin.png") no-repeat;
+  display: flex;
+  .bg{
+    flex: 1;
+    background-size: cover;
+    background-position: center;
+    background-size: 800px;
+    background-repeat: no-repeat;
+    background-image: url("../assets/img/Login-left2.png");
+  }
+  .login-panel{
+    width: 430px;
+    margin-right: 15%;
+    margin-top: calc((100vh - 500px) / 2);
+  }
+}
 </style>
